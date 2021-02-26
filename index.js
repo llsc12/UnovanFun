5842d8c3297b3a0e6fa5677b20ceb267a8dcf453
llsc12 <42747613+llsc12@users.noreply.github.com>
2021-02-26 19:16:10 +0000

    im done

--- index.js
+++ index.js
@@ -2,44 +2,76 @@ const discord = require("discord.js"), fs = require("fs"), Discord = require("di
 const client = new Discord.Client({ messageSweepInterval: 60, disableEveryone: true }) // Create a client
 const {bot_token, prefix} = require('./config.json')
 const fetch = require('node-fetch');
-
-client.commands = new Discord.Collection(); // Creates a code collection
-
-client.on("ready", async () => { // Logs response when started
-  console.log(`bonk`);
-});
-
-let commands = {} // A bit of pre-work to set all things up
-fs.readdir("./commands/", (err, files) => {
-  if (err) console.error(err);
-  console.log(files)
-  for (var file of files) if (file.endsWith(".js")) {
-    commands[file.replace(".js", "")] = `./commands/${file}`;
-    let props = require(`./commands/${file}`);
-    client.commands.set(props.help.name, props);
-  }
-});
+let valid = new Array();
+valid = ['8ball', 'Random_hentai_gif', 'meow', 'erok', 'lizard', 'feetg', 'baka', 'v3', 'bj', 'erokemo', 'tickle', 'feed', 'neko', 'kuni', 'femdom', 'futanari', 'smallboobs', 'goose', 'nekoapi_v3.1', 'poke', 'les', 'trap', 'pat', 'boobs', 'blowjob', 'hentai', 'hololewd', 'ngif', 'fox_girl', 'wallpaper', 'lewdk', 'solog', 'pussy', 'yuri', 'lewdkemo', 'lewd', 'anal', 'pwankg', 'nsfw_avatar', 'eron', 'kiss', 'pussy_jpg', 'woof', 'hug', 'keta', 'cuddle', 'eroyuri', 'slap', 'cum_jpg', 'waifu', 'gecg', 'tits', 'avatar', 'holoero', 'classic', 'kemonomimi', 'feet', 'gasm', 'spank', 'erofeet', 'ero', 'solo', 'cum', 'smug', 'holo', 'nsfw_neko_gif']
 
 client.on("message", async message => {
   if (!message.guild || message.author.bot) return;
+  if (message.content.startsWith(prefix)) {
+    message.delete()
+    let bonk = message.content.split(prefix)
+    let args = bonk[1].split(" ");
+    let command = args.shift().toLowerCase();
+    if (command == 'neko') {
+      if (message.channel.topic) {
+        if (!message.channel.topic.includes('NSFW')) {
+          if (!message.channel.nsfw) {
+            let nembed = new discord.MessageEmbed()
+            .addField('bruh, think about the children','If this was supposed to work, set channel to NSFW or include NSFW in channel topic')
+            .setColor('GREEN')
+            .setFooter('Requested by '+message.author.tag, message.author.displayAvatarURL({dynamic: true}));
+            message.channel.send(nembed)
+            return;
+          }
+        }
+      } else if (!message.channel.nsfw) {
+        let nembed = new discord.MessageEmbed()
+        .addField('bruh, think about the children','If this was supposed to work, set channel to NSFW or include NSFW in channel topic')
+        .setColor('GREEN')
+        .setFooter('Requested by '+message.author.tag, message.author.displayAvatarURL({dynamic: true}));
+        message.channel.send(nembed)
+        return;
+      }
+      
+      if (!args[0]) {
+        args = ['neko']
+      }
+      if (args[0]) {
+        var n = valid.includes(args[0])
+        if (n == false) {
+          let nembed = new discord.MessageEmbed()
+          .setTitle('Invalid argument')
+          .setColor('RED')
+          .addField('Here are valid arguments', String(valid))
+          .setFooter('Requested by '+message.author.tag, message.author.displayAvatarURL({dynamic: true}));
+          message.channel.send(nembed)
+          return;
+        }
+      }
+      fetch(`https://nekos.life/api/v2/img/${args[0]}`)
+        .then(res => res.json())
+        .then(json => {
+          let nembed = new discord.MessageEmbed()
+          .setTitle(args[0])
+          .setURL(json.url)
+          .setImage(json.url)
+          .setColor('BLUE')
+          .setFooter('Requested by '+message.author.tag, message.author.displayAvatarURL({dynamic: true}));
+          message.channel.send(nembed)
+        });
+    }
 
-  if (message.content.startsWith(prefix) || message.content.match(`^<@!?${client.user.id}> `)) {
-    let args = message.content.split(" ");
-    if (args[0].match(`^<@!?${client.user.id}>`)) args.shift(); else args[0] = args[0].slice(prefix.length);
-    let command = args.shift().toLowerCase()
+    if (command == 'nsfwtest') {
+      message.channel.send(message.channel.nsfw)
+    }
+  }
+});
 
-    if (commands[command]) try {
-      let commandFile = require(commands[command])
-
-      if (getPermissionLevel(message.member) < commandFile.permissionRequried) return message.channel.send(`❌ You don't have permission! For help type \`${prefix}help\`.`); // Starting some basic commands.
-      commandFile.run(client, message, args, config, queue)
-    } catch(e) {}
-  } else if (message.content.match(`^<@!?${client.user.id}>`)) return message.channel.send(`👋 My prefix is \`${prefix}\`. Commands are ${Object.keys(commands).map(c => `\`${prefix}${c}\``).join(", ")}.`); // Says this when pinged.
+client.on("ready", () => {
+client.user.setPresence({status: "dnd"})
+client.user.setActivity('imagine being horny smh', { type: 'PLAYING' });
+console.log('ready')
 })
 
-
-fetch('https://nekos.life/api/v2/endpoints')
-    .then(res => res.json())
-    .then(json => console.log(json))
 
 client.login(bot_token)
